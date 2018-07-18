adefa9cd8705502646184d97cd1f3e655c076d9d
Tim Caldwell <tim.caldwell@einti.com>
2018-07-18 16:14:46 +1000

    add images to import from api

--- app/jobs/fetch_pets_job.rb
+++ app/jobs/fetch_pets_job.rb
@@ -1,5 +1,6 @@
 class FetchPetsJob < ApplicationJob
   queue_as :default
+
   def fetch(api_url) 
     response =  HTTParty.get api_url, format: :plain
     JSON::parse response, symbolize_names: true
@@ -7,20 +8,19 @@ class FetchPetsJob < ApplicationJob
 
   def perform
     api_url = 'https://au-pet-api.herokuapp.com/'
-    api_url = 'http://localhost:3000'
+    api_url = 'http://localhost:3000/'
+    
+    # fetch main content of the pets api
     content = fetch api_url
 
     content.each do |p|
       
-      # TODO: Extract "lookup data"
-      # type_name (eg 'cat')
-      #  animal_type_id :integer
-      #  shelter_id     :string
-      #  breed_id       :bigint(8)
+      #extract lookup data and create records where necessary
       animal_type = AnimalType.find_or_create_by :type_name => p[:type_name]
       breed = Breed.find_or_create_by :name => p[:breedPrimary]
       breed.animal_type_id = animal_type.id unless breed.animal_type_id.present?
 
+      # create (or find an existing) pet and set details
       pet = Pet.find_or_create_by :api_id => p[:id]
       pet.name = p[:name]
       pet.colour = [p[:primary_colour],p[:secondary_colour]].join " / "
@@ -28,12 +28,25 @@ class FetchPetsJob < ApplicationJob
       pet.size = p[:size]
       pet.vacc_status = p[:isVaccinated]
 
-      pet.shelter = Shelter.first
-
+      # set related items
+      pet.shelter = Shelter.first # Shelter.first just for testing TODO: Get shelters from API
       pet.animal_type = animal_type
       pet.breed = breed
 
       pet.save
+
+      # fetch images for this pet from the API
+      images = fetch "#{api_url}pets/#{p[:id]}"
+      
+      if images.kind_of? Array
+        #add each image to the pet
+        images.each do |img|
+          pet.pet_pics.find_or_create_by :url => img[:image_path]
+        end
+      elsif images.present?
+        pet.pet_pics.find_or_create_by :url => images[:image_path]
+      end
+
 
     end
   end
