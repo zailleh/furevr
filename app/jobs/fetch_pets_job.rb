502d037fda3892a1bf865aa2d6cf86d635a48d3f
Tim Caldwell <tim.caldwell@einti.com>
2018-07-20 13:04:20 +1000

    fix failure in fetch pets job

--- app/jobs/fetch_pets_job.rb
+++ app/jobs/fetch_pets_job.rb
@@ -12,24 +12,28 @@ class FetchPetsJob < ApplicationJob
     # fetch main content of the pets api
     begin
       content = fetch api_url
+      # content.each_with_index do |p,i|
+      #   puts "#{i}: #{p[:id]}"
+      # end
+      puts "fetched data from #{api_url}"
 
       content.each do |p|
         #extract lookup data and create records where necessary
-        animal_type = AnimalType.find_or_create_by :type_name => p[:type_name].strip
-        breed = Breed.find_or_create_by :name => p[:breedPrimary].strip
+        animal_type = AnimalType.find_or_create_by :type_name => p[:type_name]
+        breed = Breed.find_or_create_by :name => p[:breedPrimary]
         breed.animal_type_id = animal_type.id unless breed.animal_type_id.present?
 
         # create (or find an existing) pet and set details
-        pet = Pet.find_or_create_by :api_id => p[:id]
-        pet.name = p[:name].strip
+        pet = Pet.find_or_initialize_by :api_id => p[:id]
+        pet.name = p[:name]
         pet.colour = [p[:primary_colour],p[:secondary_colour]].join " / "
         pet.date_of_birth = p[:date_of_birth]
-        pet.size = p[:size].strip
+        pet.size = p[:size]
         pet.vacc_status = p[:isVaccinated]
-        pet.description = p[:description1].strip
-
+        pet.description = p[:description1]
+        
         # set related items
-        shelter = Shelter.find_or_create_by id: p[:shelter]
+        shelter = Shelter.find_or_initialize_by id: p[:shelter]
         unless shelter.shelter_type.present?
           shelter.shelter_type = ShelterType.find_by type_name: 'Animal Rescue' 
           shelter.save
@@ -37,10 +41,8 @@ class FetchPetsJob < ApplicationJob
         pet.shelter = shelter
         pet.animal_type = animal_type
         pet.breed = breed
-
+        
         if pet.save 
-
-          # fetch images for this pet from the API
           images = fetch "#{api_url}pets/#{p[:id]}"
           
           if images.kind_of? Array
@@ -55,10 +57,12 @@ class FetchPetsJob < ApplicationJob
           puts "PET NOT SAVE!!!!"
         end
       end # end each pet from api
-    rescue
-      "Something went wrong connecting to API"
-    end
 
-      FetchPetsJob.set(wait: 1.hour).perform_later
+      puts "#{content.count} pets processed from API "
+    rescue  => e
+      puts "Something went wrong connecting to API"
+      puts e
+    end
+    FetchPetsJob.set(wait: 1.hour).perform_later
   end
 end
